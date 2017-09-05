19fa360a41e7e7ce3e6b99ea20fb5f6c9e8887cc
peterliuxb <woairuidaren@gmail.com>
2017-09-05 16:28:21 +0800

    finish actions reduces store

--- src/components/App.js
+++ src/components/App.js
@@ -2,45 +2,14 @@ import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Route,
-  Link
+  Switch
 } from 'react-router-dom'
-
-import * as api from '../utils/api'
-import Post from './Post'
-import Category from './Category'
+import { connect } from 'react-redux'
+import * as readableApi from '../utils/api'
+import { withRouter } from 'react-router'
 
 class App extends Component {
 
-  state = {
-    posts:[],
-    categories:[],
-    comments:[]
-  }
-
-  componentDidMount() {
-    api.getPost().then((posts) => {
-      console.log("get post when mount", posts)
-      this.setState({ posts })
-    });
-
-    
-
-    api.getCategory().then((categories) => {
-      //console.log("From api:", categories)
-      this.setState({categories: categories})
-    })
-  };
-
-
-
-  render() {
-    return (
-      <div>
-        <Category categories={this.state.categories}/>
-        <Post posts={this.state.posts} />
-      </div>
-    );
-  }
 }
 
 export default App;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,22 @@
 import { combineReducers } from 'redux'
-import { ADD_POST, ADD_COMMENT } from '../actons'
+import { SHOW_MESSAGE } from '../actions'
+import { categories } from './categories'
+import { comments } from './comments'
+import { posts }  from './posts'
 
-const initialPostState = {
-  
+function showMessage(state='',action){
+  switch(action.type){
+    case SHOW_MESSAGE:
+      const { message } = action
+      return message
+    default:
+      return state
+  }
 }
+
+export default combineReducers({
+  categories,
+  posts,
+  comments,
+  showMessage
+})
