ed4986b05d7586094de45976d983e484e513b3cf
peterliuxb <woairuidaren@gmail.com>
2017-11-19 09:45:44 +0800

    extrat posts from app component

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,7 @@ import {
 } from '../utils/readableAPI'
 import Categories from './Categories'
 import AddPost from './AddPost'
+import Posts from './posts'
 
  const objectToArray = obj => {
   if (obj) return Object.keys(obj).map(key => obj[key])
@@ -37,7 +38,7 @@ class App extends Component {
 
       <div className="container">
         <Row className="show-grid">
-        {console.log(posts)}
+        
         <Col xs={9} md={6}>
           <div className="navbar-brand">
             <img src={LogoImg} width="112" height="36" alt="This a logo of redux project"/>
@@ -53,118 +54,17 @@ class App extends Component {
         </Row>
         <hr />
         <Row className="show-grid">
-        <div className="reabable-category">
-          <h2>Categories</h2>
-          <Categories categories={categories}/>
-
-        </div>
-        </Row>
-
-        <hr />
-
-        {posts.length > 0 && posts[0].map((post) => (
-          <div className="container content has-top-margin"
-            style={{ marginBottom: '50px' }}>
-            <div className="columns">
-
-              <div className="column">
-                <article className="media">
-
-                  <div className="media-left">
-                    <figure className="has-text-centered">
-                    <div className="column" style={{ maxWidth: '115px' }}>
-                      <div className="readable-voteScore-wrapper">
-                        <div className={'readable-voteScore-value notification '}>
-                              {post.voteScore}
-                          </div>
-                        <a className="button is-success is-outlined"
-                          onClick={() => {}}>
-                          <i className="fa fa-thumbs-o-up" aria-hidden="true" />
-                        </a>
-                        <a className="button is-danger is-outlined"
-                          onClick={() => {}}>
-                          <i className="fa fa-thumbs-o-down" aria-hidden="true" />
-                        </a>
-                      </div>
-                     </div>
-                     </figure>
-                   </div>
-
-                  <div className="media-content">
-
-                    <div className="content">
-                      <p>
-                        <strong>
-                        <i
-                        className="fa fa-user-circle-o"
-                        aria-hidden="true"
-                        />{' '}
-                        {post.author}
-                        </strong>
-                          &nbsp; · &nbsp;
-                        <small>
-                        <i className="fa fa-clock-o" aria-hidden="true" />{' '}
-                        {Date(post.timestamp)}
-                        </small>
-                        <br />
-                        <a
-                        className="is-size-4"
-                        >
-                        {post.title}
-                        </a>
-                      </p>
-                    </div>
-
-                    <nav className="level is-mobile">
-                    <div className="level-left">
-                      <a className="tag">
-                        {post.category}
-                      </a>
-                      &nbsp;
-                      <span className="icon is-small">
-                        <i className="fa fa-comment-o" />
-                      </span>
-                      &nbsp;
-                      {posts[1].map(comment => (
-                        <p>{comment.author}</p>
-                      ))}
-                    </div>
-                  </nav>
-
-                  </div>
-
-                </article>
-              </div>
-
-              <div className="column" style={{ maxWidth: '100px' }}>
-              <div className="button actionButtonFromPostList is-danger is-small is-outlined"
-                onClick={() => {}}>
-                <span className="icon is-small">
-                  <i className="fa fa-trash-o" />
-                </span>
-                  &nbsp; delete
-              </div>
-              <br />
-              <a className="button actionButtonFromPostList is-small is-info is-outlined">
-                <span className="icon is-small">
-                  <i className="fa fa-edit" />
-                </span>
-                &nbsp; edit
-              </a>
-              </div>
-
-            </div>
+          <div className="reabable-category">
+            <h2>Categories</h2>
+            <Categories categories={categories}/>
           </div>
-
-        ))}
-
+        </Row>
+        <hr />
+        <Posts posts={posts}/>
         <hr />
         <Switch>
-
           <Route exact path ='/new' component={AddPost} />
-
         </Switch>
-        <div className="container">footer</div>
       </div>
 
     );
--- src/components/posts.js
+++ src/components/posts.js
@@ -4,12 +4,110 @@ import Row  from 'react-bootstrap/lib/Row'
 import Col from 'react-bootstrap/lib/Col'
 import { connect } from 'react-redux'
 
-class posts extends Component {
+class Posts extends Component {
   render() {
+    const {posts} = this.props
     return(
       <div className="readable-posts">
-        
-      </div>
+      {posts.length > 0 && posts[0].map((post) => (
+        <div className="container content has-top-margin"
+          style={{ marginBottom: '50px' }}>
+          <div className="columns">
+
+            <div className="column">
+              <article className="media">
+
+                <div className="media-left">
+                  <figure className="has-text-centered">
+                  <div className="column" style={{ maxWidth: '115px' }}>
+                    <div className="readable-voteScore-wrapper">
+                      <div className={'readable-voteScore-value notification '}>
+                            {post.voteScore}
+                        </div>
+                      <a className="button is-success is-outlined"
+                        onClick={() => {}}>
+                        <i className="fa fa-thumbs-o-up" aria-hidden="true" />
+                      </a>
+                      <a className="button is-danger is-outlined"
+                        onClick={() => {}}>
+                        <i className="fa fa-thumbs-o-down" aria-hidden="true" />
+                      </a>
+                    </div>
+                   </div>
+                   </figure>
+                 </div>
+
+                <div className="media-content">
+
+                  <div className="content">
+                    <p>
+                      <strong>
+                      <i
+                      className="fa fa-user-circle-o"
+                      aria-hidden="true"
+                      />{' '}
+                      {post.author}
+                      </strong>
+                        &nbsp; · &nbsp;
+                      <small>
+                      <i className="fa fa-clock-o" aria-hidden="true" />{' '}
+                      {Date(post.timestamp)}
+                      </small>
+                      <br />
+                      <a
+                      className="is-size-4"
+                      >
+                      {post.title}
+                      </a>
+                    </p>
+                  </div>
+
+                  <nav className="level is-mobile">
+                  <div className="level-left">
+                    <a className="tag">
+                      {post.category}
+                    </a>
+                    &nbsp;
+                    <span className="icon is-small">
+                      <i className="fa fa-comment-o" />
+                    </span>
+                    &nbsp;
+                    {posts[1].map(comment => (
+                      <p>{comment.author}</p>
+                    ))}
+                  </div>
+                </nav>
+
+                </div>
+
+              </article>
+            </div>
+
+            <div className="column" style={{ maxWidth: '100px' }}>
+            <div className="button actionButtonFromPostList is-danger is-small is-outlined"
+              onClick={() => {}}>
+              <span className="icon is-small">
+                <i className="fa fa-trash-o" />
+              </span>
+                &nbsp; delete
+            </div>
+            <br />
+            <a className="button actionButtonFromPostList is-small is-info is-outlined">
+              <span className="icon is-small">
+                <i className="fa fa-edit" />
+              </span>
+              &nbsp; edit
+            </a>
+            </div>
+
+          </div>
+        </div>
+
+
+      ))}
+        </div>
     )
   }
 }
+
+export default Posts
