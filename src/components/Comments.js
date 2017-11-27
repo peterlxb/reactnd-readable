3b228b23e87ed022e205d6a52d178bcfaf6e4b06
peterliuxb <woairuidaren@gmail.com>
2017-11-27 13:12:29 +0800

    add comments component

---
+++ src/components/Comments.js
@@ -0,0 +1,60 @@
+import React,{Component} from 'react'
+
+class Comments extends Component {
+  render() {
+    const {comment} = this.props
+    return(
+      <section className="content readable-comment">
+        <div className="columns is-mobile">
+
+          <div className="column" style={{ maxWidth: '115px' }}>
+            <div className="readable-voteScore-wrapper">
+              <div className={'readable-voteScore-value notification '}>
+                  {comment.voteScore}
+              </div>
+                  <a className="button is-success is-outlined"
+                    onClick={() => {}}>
+                    <i className="fa fa-thumbs-o-up" aria-hidden="true" />
+                  </a>
+                  <a className="button is-danger is-outlined"
+                    onClick={() => {}}>
+                    <i className="fa fa-thumbs-o-down" aria-hidden="true" />
+                  </a>
+              </div>
+            </div>
+
+          <div className="column">
+          <div>
+             <strong>{comment.author}</strong>
+             &nbsp;
+             <small>{comment.timestamp}</small>
+             &nbsp; · &nbsp;
+             <span>
+              <div onClick={() => {}} className="button is-small is-danger is-outlined">
+                delete
+              </div>
+              &nbsp;
+              <div onClick={() => {}} className="button is-small is-info is-outlined">
+              edit
+              </div>
+              </span>
+             <br/>
+             <div className="content">
+              {comment.body.split('\n').map((item, key) => {
+                return (
+                  <span key={key}>
+                  {item}
+                  <br />
+              </span>
+            )
+              })}
+            </div>
+           </div>
+          </div>
+        </div>
+      </section>
+    )
+  }
+}
+
+export default Comments
--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -2,6 +2,7 @@ import React,{ Component } from 'react'
 import {Link } from 'react-router-dom'
 import {connect} from 'react-redux'
 import { deletePosts } from '../actions/posts'
+import Comments from './Comments'
 
 const objectToArray = obj => {
  if (obj) return Object.keys(obj).map(key => obj[key])
@@ -94,55 +95,7 @@ class SinglePost extends Component {
                           {commentsToShow.length} comment:
                         </h3>}
                     {commentsToShow.map((comment, index) =>(
-                      <section className="content readable-comment">
-                        <div className="columns is-mobile">
-
-                          <div className="column" style={{ maxWidth: '115px' }}>
-                            <div className="readable-voteScore-wrapper">
-                              <div className={'readable-voteScore-value notification '}>
-                                  {comment.voteScore}
-                              </div>
-                                  <a className="button is-success is-outlined"
-                                    onClick={() => {}}>
-                                    <i className="fa fa-thumbs-o-up" aria-hidden="true" />
-                                  </a>
-                                  <a className="button is-danger is-outlined"
-                                    onClick={() => {}}>
-                                    <i className="fa fa-thumbs-o-down" aria-hidden="true" />
-                                  </a>
-                              </div>
-                            </div>
-
-                          <div className="column">
-                          <div>
-                             <strong>{comment.author}</strong>
-                             &nbsp;
-                             <small>{comment.timestamp}</small>
-                             &nbsp; · &nbsp;
-                             <span>
-                              <div onClick={() => {}} className="button is-small is-danger is-outlined">
-                                delete
-                              </div>
-                              &nbsp;
-                              <div onClick={() => {}} className="button is-small is-info is-outlined">
-                              edit
-                              </div>
-                              </span>
-                             <br/>
-                             <div className="content">
-                              {comment.body.split('\n').map((item, key) => {
-                                return (
-                                  <span key={key}>
-                                  {item}
-                                  <br />
-                              </span>
-                            )
-                              })}
-                            </div>
-                           </div>
-                          </div>
-                        </div>
-                      </section>
+                      <Comments comment={comment}/>
                       )
                     )}
 
@@ -166,7 +119,7 @@ class SinglePost extends Component {
                       onChange={() => {}} />
                     <div className="button has-bottom-mini-margin">Post Comment</div>
 
-                    
+
                   </div>
 
             </div>
